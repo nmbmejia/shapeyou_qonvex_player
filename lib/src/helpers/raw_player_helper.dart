896492830f1568a66b09eb7bef7871eed32c1eea
Neil Marc Mejia <m.neilmarc@yahoo.com>
2026-03-04 06:52:56 +0800

    Update 1.4.1

--- lib/src/helpers/raw_player_helper.dart
+++ lib/src/helpers/raw_player_helper.dart
@@ -59,11 +59,11 @@ mixin class RawPlayerHelper {
 
         .mute-button {
             position: absolute;
-            bottom: 16px;
-            right: 16px;
-            width: 40px;
-            height: 40px;
-            background: rgba(0, 0, 0, 0.6);
+            bottom: 45px;
+            right: 10px;
+            width: 50px;
+            height: 50px;
+            background: rgba(0, 0, 0, 0.1);
             border-radius: 50%;
             border: none;
             cursor: pointer;
@@ -71,16 +71,17 @@ mixin class RawPlayerHelper {
             align-items: center;
             justify-content: center;
             z-index: 1000;
-            transition: background 0.3s ease;
+            transition: background 0.2s ease;
+            padding: 10px;
         }
 
-        .mute-button:hover {
-            background: rgba(0, 0, 0, 0.8);
+        .mute-button:active {
+            background: rgba(0, 0, 0, 0.2);
         }
 
         .mute-button svg {
-            width: 20px;
-            height: 20px;
+            width: 25px;
+            height: 25px;
             fill: white;
         }
 </style>
@@ -101,7 +102,7 @@ mixin class RawPlayerHelper {
             ${showMuteButton ? '''
             <button class="mute-button" id="muteButton" onclick="toggleMute()">
               <svg id="volumeIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
+                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z" fill="none" stroke="currentColor" stroke-width="0"/>
               </svg>
             </button>
             ''' : ''}
@@ -137,9 +138,9 @@ mixin class RawPlayerHelper {
             var volumeIcon = document.getElementById('volumeIcon');
             if (volumeIcon) {
                 if (muted) {
-                    volumeIcon.innerHTML = '<path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>';
+                    volumeIcon.innerHTML = '<path d="M3.63 3.63c-.39.39-.39 1.02 0 1.41L7.29 8.7 7 9H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71v-4.17l4.18 4.18c-.49.37-1.02.68-1.6.91-.36.15-.58.53-.58.92 0 .72.73 1.18 1.39.91.8-.33 1.55-.77 2.22-1.31l1.34 1.34c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L5.05 3.63c-.39-.39-1.02-.39-1.42 0zM19 12c0 .82-.15 1.61-.41 2.34l1.53 1.53c.56-1.17.88-2.48.88-3.87 0-3.83-2.4-7.11-5.78-8.4-.59-.23-1.22.23-1.22.86v.19c0 .38.25.71.61.85C17.18 6.54 19 9.06 19 12zm-8.71-6.29l-.17.17L12 7.76V6.41c0-.89-1.08-1.33-1.71-.7zM16.5 12c0-1.77-1.02-3.29-2.5-4.03v1.79l2.48 2.48c.01-.08.02-.16.02-.24z"/>';
                 } else {
-                    volumeIcon.innerHTML = '<path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>';
+                    volumeIcon.innerHTML = '<path d="M3 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L7 9H4c-.55 0-1 .45-1 1zm13.5 2c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 4.45v.2c0 .38.25.71.6.85C17.18 6.53 19 9.06 19 12s-1.82 5.47-4.4 6.5c-.36.14-.6.47-.6.85v.2c0 .63.63 1.07 1.21.85C18.6 19.11 21 15.84 21 12s-2.4-7.11-5.79-8.4c-.58-.23-1.21.22-1.21.85z"/>';
                 }
             }
         }
